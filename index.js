836e75a31b5f7b12be77063e99b6def074a44858
Jay DeLuca <jaydeluca4@gmail.com>
2017-11-11 18:09:17 -0500

    all aboard the express train

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+// App setup
+var app = express();
+
+var server = app.listen(4000, function() {
+  console.log('listening on Port 4000');
+});
+
+// Static files
+app.use(express.static('public'));
