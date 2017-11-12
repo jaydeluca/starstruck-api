5cf3c86c1c616c3fbb2b919261c47667aa1671e5
Jay DeLuca <jaydeluca4@gmail.com>
2017-11-12 14:58:10 -0500

    lets start sending user data

--- index.js
+++ index.js
@@ -18,9 +18,53 @@ io.on('connection', function(socket) {
   console.log('made socket connection', socket.id);
 })
 
-var tick = 0;
+
+
+// Users
+const users = [
+  {
+    name: 'Player1',
+    units: {
+      wraith: 100,
+      frigate: 50,
+      astrodrone: 30
+    },
+    plunder: {
+      asteroids: {
+        platinum: 0,
+        crystal: 0,
+        uninitiated: 0
+      }
+    }
+  },
+  {
+    name: 'Player 2',
+    units: {
+      turret: 50,
+      wraith: 30,
+      frigate: 40
+    },
+    assets: {
+      asteroids: {
+        platinum: 76,
+        crystal: 40,
+        uninitiated: 3
+      },
+      resources: {
+        platinum: 394829,
+        crystal: 390239
+      }
+    }
+  }
+];
+
+
+
+var tick = {
+  id: 0,
+  users
+};
 setInterval(function () {
-  tick++;
+  tick.id++;
   io.emit('tick', tick)
-  console.log(tick);
 }, 3000);
